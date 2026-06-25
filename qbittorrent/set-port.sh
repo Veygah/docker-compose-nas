60803c4d9a0a5b9f9e589537968cb1a6979a9ff3
atawil <andrew.tawil@hotmail.com>
2026-06-24 22:02:54 -0400

    Fixing port script

--- qbittorrent/set-port.sh
+++ qbittorrent/set-port.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
 PORT=$(cat /gluetun-tmp/forwarded_port)
 echo "Using forwarded port: $PORT"
-qbittorrent-nox --profile=/config --torrenting-port=$PORT
+
+/usr/bin/qbittorrent-nox --profile=/config --torrenting-port=$PORT
